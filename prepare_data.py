37eb5788614cda735bb1a69a786eb7d1d15e46c3
Kylan Thomson <46757572+KylanThomson@users.noreply.github.com>
2022-10-28 20:24:17 +0000

    logic for preparing data for ML models

--- prepare_data.py
+++ prepare_data.py
@@ -8,20 +8,26 @@ from sklearn.preprocessing import LabelEncoder
 from sklearn.preprocessing import StandardScaler
 from sklearn.model_selection import train_test_split
 
-# loads data from csv
-# handles missing values
-# encodes categorical data
-# encodes labels
-# splits data -> x_train, x_test, y_train, y_test
-# (optional) Feature Scaling in the form of Standardisation or Normalisation
-# Note: normalization is recommended when you have a normal distribution, standarisation will always work for feature scaling
+'''
+- loads data from csv
+- flags columns containing numerical values for handling missing values
+- handles missing values
+- encodes categorical data
+- encodes labels
+- splits data -> x_train, x_test, y_train, y_test
+- (optional) Feature Scaling in the form of Standardisation or Normalisation
+
+Note: normalization is recommended when you have a normal distribution, standardisation will always work for feature scaling
+'''
+
 
 def preprocessing():
-  # import the dataset as a dataframe object
+  # import .csv dataset as a numpy array object
     dataset = pd.read_csv('Data.csv')
     # x is independent variable, where iloc[:, :-1] everything except the last column
     x = dataset.iloc[:, :-1].values
-    x[:, 1:3] = handle_missing_data(x[:, 1:3])
+    flagged_columns = flag_number_columns(x)
+    x[:, flagged_columns[0]:flagged_columns[1] + 1] = handle_missing_data( x[:, flagged_columns[0]:flagged_columns[1] + 1])
     x = onehot_encoding(x, 0)
     # y is dependent variables (last column by convention)
     y = dataset.iloc[:, -1].values
@@ -32,7 +38,6 @@ def preprocessing():
 
 def handle_missing_data(data):
     imputer = SimpleImputer(missing_values=np.nan, strategy='mean')
-    # todo: add logic that identifies all numerical columns
     imputer.fit(data[:])
     return imputer.transform(data[:])
 
@@ -57,3 +62,12 @@ def feature_scaling(x_train, x_test):
   x_train[:, 3:] = sc.fit_transform(x_train[:, 3:])
   x_test[:, 3:] = sc.transform(x_test[:, 3:])
   return x_train, x_test
+
+def flag_number_columns(data):
+  first_row = data[1, ...]
+  flagged_columns = []
+  for i in range(len(first_row)):
+    if type(first_row[i]) == int or type(first_row[i]) == float:
+      flagged_columns.append(i)
+  print(flagged_columns)
+  return flagged_columns
